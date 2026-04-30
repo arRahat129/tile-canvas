8dbcaa179b86619510da5af84c49ffb4a6621456
arRahat129 <rahashik129@gmail.com>
2026-04-30 21:47:15 +0600

    changed client baseURL

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://tile-canvas.vercel.app"
 })
 
 export const { signIn, signUp, signOut, updateUser, useSession } = createAuthClient()
