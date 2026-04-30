ecd35f65107d3e63bacf060b3de98dc5d214582b
arRahat129 <rahashik129@gmail.com>
2026-04-30 20:45:08 +0600

    to do google authorization, i am deploying the site, and facing error, to avoid that, I just used this, that will work as diversion

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,14 +1,36 @@
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient(process.env.TILE_CANVAS_DB_URI);
+// const db = client.db("tile-canvas");
+
+// export const auth = betterAuth({
+//     database: mongodbAdapter(db, {
+//         client
+//     }),
+//     emailAndPassword: {
+//         enabled: true,
+//     },
+// });
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.TILE_CANVAS_DB_URI);
-const db = client.db("tile-canvas");
+const uri = process.env.TILE_CANVAS_DB_URI;
+
+let dbAdapter = undefined;
+
+if (uri) {
+    const client = new MongoClient(uri);
+    const db = client.db("tile-canvas");
+
+    dbAdapter = mongodbAdapter(db, { client });
+}
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db, {
-        client
-    }),
+    database: dbAdapter, // will be undefined if no URI
     emailAndPassword: {
         enabled: true,
     },
